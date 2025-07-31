32f6dd34569533994db6884cb37bc9b5d7cdfe0a
MuhammadZain28 <zaina.azhar2005@gmail.com>
2025-07-31 16:04:41 +0500

    Add files via upload

--- mobile/components/Loading.tsx
+++ mobile/components/Loading.tsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { ActivityIndicator, View, StyleSheet } from "react-native";
+import { ActivityIndicator, View, StyleSheet, ImageBackground } from "react-native";
 const Loading: React.FC = () => {
     return (
-        <View style={styles.container}>
-            <ActivityIndicator size="large" color="#676767ff" />
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ImageBackground source={require('../assets/images/splash-icon.png')} resizeMode="contain" style={{ flex: 1, justifyContent: 'center', alignItems: 'center', position: 'absolute', width: '100%', height: '100%', opacity: 0.5 }}>
+                <View style={styles.container}>
+                    <ActivityIndicator size="large" color="#676767ff" />
+                </View>
+            </ImageBackground>
         </View>
     );
 };
@@ -12,7 +16,8 @@ export default Loading;
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#dbdbdb7e',
+        width: "100%",
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
         justifyContent: "center",
         alignItems: "center"
     }
